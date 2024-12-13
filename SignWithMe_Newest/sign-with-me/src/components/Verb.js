d8424f7e9be58fc750535797d8b232ccd9e1a9b7
RyancRiv <104662361+RyancRiv@users.noreply.github.com>
2024-12-13 16:31:45 -0500

    Update Verb.js

--- SignWithMe_Newest/sign-with-me/src/components/Verb.js
+++ SignWithMe_Newest/sign-with-me/src/components/Verb.js
@@ -188,7 +188,7 @@ const Quiz = () => {
       if (userName) {
         // Send a PUT request to update the lesson with quiz_complete = true
         axios
-          .put("http://localhost:5000/api/lessons/update-lesson", {
+          .put("https://signwithme-92dm.onrender.com/api/lessons/update-lesson", {
             lessonId: "203",
             userName: userName,
             quiz_complete: true,
